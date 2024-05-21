9e2758f411c4cfb463619f4eb77a917225b20d9e
Alex Amarandei <alex.m.amarandei@gmail.com>
2024-05-21 14:25:45 +0300

    :art: configure prettier and eslint

--- gulo-fe/src/api/fetchNftDetails.ts
+++ gulo-fe/src/api/fetchNftDetails.ts
@@ -1,21 +1,21 @@
-import tokenUriAbi from "@/constants/abi/sablier";
-import Stream from "@/interfaces/stream";
-import { useReadContract } from "wagmi";
+import tokenUriAbi from '@/constants/abi/sablier';
+import Stream from '@/interfaces/stream';
+import { useReadContract } from 'wagmi';
 
-const mockJsonWithImageFieldBase64 = "eyJpbWFnZSI6ICIifQ=="; // {"image":""}
+const mockJsonWithImageFieldBase64 = 'eyJpbWFnZSI6ICIifQ=='; // {"image":""}
 
 export default function fetchNftDetails(stream: Stream) {
-	const { data: name } = useReadContract({
-		abi: tokenUriAbi,
-		address: stream.contract.address,
-		functionName: "tokenURI",
-		chainId: Number(stream.chainId),
-		args: [stream.tokenId],
-	});
+  const { data: name } = useReadContract({
+    abi: tokenUriAbi,
+    address: stream.contract.address,
+    functionName: 'tokenURI',
+    chainId: Number(stream.chainId),
+    args: [stream.tokenId]
+  });
 
-	const infoBase64 = name?.replace("data:application/json;base64,", "") ?? mockJsonWithImageFieldBase64;
-	const infoBase64Decoded = JSON.parse(Buffer.from(infoBase64, "base64").toString("utf-8"));
-	const nftDetailsBase64 = infoBase64Decoded["image"];
+  const infoBase64 = name?.replace('data:application/json;base64,', '') ?? mockJsonWithImageFieldBase64;
+  const infoBase64Decoded = JSON.parse(Buffer.from(infoBase64, 'base64').toString('utf-8'));
+  const nftDetailsBase64 = infoBase64Decoded['image'];
 
-	return nftDetailsBase64;
+  return nftDetailsBase64;
 }
