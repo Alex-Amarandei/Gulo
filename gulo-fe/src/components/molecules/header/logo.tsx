1504baca44c9ff3307c5c447d730dc7f606b61bf
Alex Amarandei <alex.m.amarandei@gmail.com>
2024-05-24 15:30:27 +0300

    :lipstick: increase logo font size

--- gulo-fe/src/components/molecules/header/logo.tsx
+++ gulo-fe/src/components/molecules/header/logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <div className="flex items-center space-x-2 flex-1 justify-start">
       <Image src="/images/branding/logo.png" alt="App Logo" width={40} height={40} className="cursor-default" />
-      <span className="text-white text-xl font-bold cursor-default">Gulo</span>
+      <span className="text-white text-2xl font-bold cursor-default">Gulo</span>
     </div>
   );
 }
