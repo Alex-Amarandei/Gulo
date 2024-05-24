a7e0de02cd46df79a8187a28a5ca9135c6167716
Alex Amarandei <alex.m.amarandei@gmail.com>
2024-05-24 14:41:37 +0300

    :egg: add Not Found Page

---
+++ gulo-fe/src/app/not-found.tsx
@@ -0,0 +1,15 @@
+const NotFoundPage = () => {
+  return (
+    <div style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/o-YBDTqX_ZU?autoplay=1&mute=0"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen></iframe>
+    </div>
+  );
+};
+
+export default NotFoundPage;
