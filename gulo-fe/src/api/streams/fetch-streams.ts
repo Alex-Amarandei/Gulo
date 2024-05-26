6a891d5cd62078759c8d2f6fcf19dccd2c002f4a
Alex Amarandei <alex.m.amarandei@gmail.com>
2024-05-26 14:13:03 +0300

    :sparkles: add support for multiple chains

--- gulo-fe/src/api/streams/fetch-streams.ts
+++ gulo-fe/src/api/streams/fetch-streams.ts
@@ -1,9 +1,9 @@
 import GET_STREAMS_QUERY from '@/constants/get-streams-query';
-import WAGMI_CONFIG from '@/utils/configs';
+import WAGMI_CONFIG, { getSablierSubgraphEndpoint } from '@/utils/configs';
 import { getAccount } from '@wagmi/core';
 
 const fetchStreams = async () => {
-  const address = getAccount(WAGMI_CONFIG).address;
+  const { address, chain } = getAccount(WAGMI_CONFIG);
 
   const variables = {
     first: 31,
@@ -13,7 +13,9 @@ const fetchStreams = async () => {
     subgraphId: '1000000000',
   };
 
-  const response = await fetch('https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-v2-sepolia/', {
+  const endpoint = getSablierSubgraphEndpoint(chain);
+
+  const response = await fetch(endpoint, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
--- gulo-fe/src/utils/configs.ts
+++ gulo-fe/src/utils/configs.ts
@@ -1,11 +1,73 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { mainnet, sepolia } from 'wagmi/chains';
+import {
+  Chain,
+  arbitrum,
+  avalanche,
+  base,
+  blast,
+  bsc,
+  gnosis,
+  lightlinkPhoenix,
+  mainnet,
+  optimism,
+  polygon,
+  scroll,
+  sepolia,
+  zkSync,
+} from 'wagmi/chains';
 
 const WAGMI_CONFIG = getDefaultConfig({
   appName: 'Gulo',
   projectId: 'a6bc9e7c5a90b4c17dee323e0efc3683',
-  chains: [mainnet, sepolia],
+  chains: [
+    mainnet,
+    arbitrum,
+    avalanche,
+    base,
+    blast,
+    bsc,
+    gnosis,
+    lightlinkPhoenix,
+    optimism,
+    polygon,
+    scroll,
+    sepolia,
+    zkSync,
+  ],
   ssr: true,
 });
+
+export function getSablierSubgraphEndpoint(chain: Chain | undefined): string {
+  switch (chain) {
+    case mainnet:
+      return 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-v2/';
+    case arbitrum:
+      return 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-v2-arbitrum/';
+    case avalanche:
+      return 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-v2-avalanche/';
+    case base:
+      return 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-v2-base/';
+    case blast:
+      return 'https://api.studio.thegraph.com/query/57079/sablier-v2-blast/version/latest';
+    case bsc:
+      return 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-v2-bsc/';
+    case gnosis:
+      return 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-v2-gnosis/';
+    case lightlinkPhoenix:
+      return 'https://graph.phoenix.lightlink.io/query/subgraphs/name/lightlink/sablier-v2-lightlink';
+    case optimism:
+      return 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-v2-optimism/';
+    case polygon:
+      return 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-v2-polygon/';
+    case scroll:
+      return 'https://api.studio.thegraph.com/query/57079/sablier-v2-scroll/version/latest';
+    case sepolia:
+      return 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-v2-sepolia/';
+    case zkSync:
+      return 'https://api.studio.thegraph.com/query/57079/sablier-v2-zksync/version/latest';
+    default:
+      return 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-v2/';
+  }
+}
 
 export default WAGMI_CONFIG;
