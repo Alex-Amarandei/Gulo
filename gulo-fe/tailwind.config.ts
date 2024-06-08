3308b773c6a87ef121222a15619db298ff07d142
Alex Amarandei <alexwama@amazon.com>
2024-06-08 18:54:17 +0300

    :bug: fix(tailwind): add lib folder

--- gulo-fe/tailwind.config.ts
+++ gulo-fe/tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/lib/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   plugins: [forms, require('tailwindcss-animate')],
   prefix: '',
